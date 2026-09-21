01723b97a31288180acd58c07e40eeee51884c35
Abhishek Singh <abhisheksinghdec04@gmail.com>
2026-09-22 00:06:45 +0530

    Time: 3 ms (47.15%), Space: 44.9 MB (56%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,32 +1,14 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num: nums1){
-            if(!map.containsKey(num)) map.put(num,1);
-            else map.put(num,map.get(num)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        HashMap<Integer,Integer> gap=new HashMap<>();
-        for(int num: nums2){
-            if(!gap.containsKey(num)) gap.put(num,1);
-            else gap.put(num,gap.get(num)+1);
-        }
-        int m=nums1.length;
-        int n=nums2.length;
-        List <Integer> list=new ArrayList<>();
-        if(m>n){
-            for(int num: nums2){
-                if(map.containsKey(num)){
-                    list.add(num);
-                    map.remove(num);
-                }
-            }
-        }
-        else{
-            for(int num: nums1){
-                if(gap.containsKey(num)){
-                    list.add(num);
-                    gap.remove(num);
-                }
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                map.remove(num);
             }
         }
         int[] result = new int[list.size()];
